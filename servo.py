adba8258dcb5986a1826780057923639cd5f8e4b
Gabriel Azenha Fachim <101106957+GabrielAF-Faca@users.noreply.github.com>
2024-11-20 18:33:56 -0300

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,41 +1,5 @@
 from machine import Pin, PWM
-'''
-class Servo:
-    
-    # Construtor
-    def __init__(self, reverse=False):
-        self.reverse = reverse
-        self.FREQ = 50
-        self.pulse_min = 2000 # 0º ~ 0.61 ms
-        self.pulse_max = 7800 # 180º ~ 2.38 ms
-        self.duty = self.pulse_min + (self.pulse_max - self.pulse_min) #90º
-    
-    # Configura o pino e inicia o PWM a 90º
-    def attach(self, pino):
-        self.pin = Pin(pino)
-        self.pwm = PWM(self.pin)
-        self.pwm.freq(self.FREQ)
-        self.pwm.duty_u16(self.duty)
-    
-    # Converte o angulo em pulso
-    def convert(self, angulo):
-        if angulo <= 0:
-            return self.pulse_min
-        if angulo >= 180:
-            return self.pulse_max
-        
-        pulso = self.pulse_min + int( (angulo / 180) * (self.pulse_max - self.pulse_min) )
-        return pulso
-    
-    # Recebe o angulo e controla o servo
-    def write(self, angulo):
-        self.duty = self.convert(angulo)
-        
-        if self.reverse:
-            self.duty = self.convert(180-angulo)
-        self.pwm.duty_u16(self.duty)
 
-''' 
 class Servo:
  
     def __init__(self, pin: int or Pin or PWM, freq=50, minVal=2500, maxVal=7500, reverse=False):
